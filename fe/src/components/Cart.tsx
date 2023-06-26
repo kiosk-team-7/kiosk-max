813ccb74273d6437efa97cf3f7d2074ebf595a18
hjsong <song6240@naver.com>
2023-06-26 21:48:14 +0900

    feat #26: 같은 메뉴끼리 묶어주는 함수 추가
    - 이름이 같은 메뉴끼리 수량을 합쳐주는 함수 추가
    - 옵션이 다르더라도 수량을 합쳐줌

--- fe/src/components/Cart.tsx
+++ fe/src/components/Cart.tsx
@@ -11,13 +11,26 @@ export default function Cart({ cartItems, removeItem, removeAllItems, changePage
   // 결제 수단 고르는 모달
   // 카드결제 눌렀을 때 로딩인디케이터 띄우는 함수
   // 현금결제 눌렀을 때 현금결제 모달 띄우는 함수
+
+  const reducedItems = cartItems.reduce((acc: CartItem[], cartItem: CartItem) => {
+    const sameItem = acc.find((item) => item.id === cartItem.id);
+
+    if (sameItem) {
+      sameItem.count += cartItem.count;
+    } else {
+      acc.push({...cartItem});
+    }
+
+    return acc;
+  } , []);
+
   return (
     <section className={styles.Cart}>
       <div className={styles.ItemSection}>
         <ul className={styles.ItemContainer}>
-          {cartItems.map((cartItem) => (
-            <li className={styles.Item} key={cartItem.id}>
-              <CartItem {...cartItem} removeItem={removeItem} />
+          {reducedItems.map((item) => (
+            <li className={styles.Item} key={item.id}>
+              <CartItem {...item} removeItem={removeItem} />
             </li>
           ))}
         </ul>
