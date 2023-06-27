199b632779eb9ccaa5b844d4122406b44ce6c6b2
Mandu <rjswmtk@gmail.com>
2023-06-27 15:17:07 +0900

    커피 인기메뉴 기능 구현 (#47)
    * API 요청 URL 수정 (#38)
    
    * fix #37: API 요청 URL 수정
    
    * chore #37: db.json 키 수정
    
    * chore #37: json-server route 추가
    
    * 장바구니 '전체 취소' 기능 구현 (#39)
    
    feat #34: cartItems 전체취소 기능 구현
    
    - 전체취소 확인 모달 구현
    
    * 장바구니 컴포넌트 구현 (#42)
    
    * feat #26: 장바구니 컴포넌트 구성요소 추가
    
    - 렌더링 할 요소를 추가하고 구조를 잡음
    
    * feat #26: 장바구니 컴포넌트 레이아웃
    
    * feat #26: 같은 메뉴끼리 묶어주는 함수 추가
    
    - 이름이 같은 메뉴끼리 수량을 합쳐주는 함수 추가
    - 옵션이 다르더라도 수량을 합쳐줌
    
    * refactor: API url 수정 및 변수명 수정
    
    - 오해의 소지가 있는 beverages를 kioskData로 변경
    
    * 결제 방식 모달 구현 (#41)
    
    * feat #40: 결제 방식 모달 구현
    
    - Modal 컴포넌트 분리
    
    * Merge remote-tracking branch 'upstream/dev' into payment-selection-modal
    
    * 장바구니 개별 항목 삭제 기능 추가 (#44)
    
    feat #43: 장바구니 개별 항목 삭제 기능 추가
    
    * feat #46: 커피 인기메뉴 기능 추가
    
    ---------
    
    Co-authored-by: khundi <albert5428@gmail.com>
    Co-authored-by: Jello <60080167+hjsong333@users.noreply.github.com>
    Co-authored-by: hjsong <song6240@naver.com>

--- fe/src/components/Cart.tsx
+++ fe/src/components/Cart.tsx
@@ -1,31 +1,130 @@
+import { useState } from "react";
+import { PaymentSelectionModal } from "./Payment";
+import styles from "./Cart.module.css";
+import Modal from "./Modal";
+
 interface CartProps {
   cartItems: CartItem[];
   removeItem: (id: number) => void;
-  removeAllItems: (id: number) => void;
+  removeAllItems: () => void;
   changePage: (path: Path) => void;
 }
 
 export default function Cart({ cartItems, removeItem, removeAllItems, changePage }: CartProps) {
-  // 결제 수단 고르는 모달
+  const [isPaymentModalOpen, setIsPaymentModalOpen] = useState(false);
+  const [isRemoveAllItemsModalOpen, setIsRemoveAllItemsModalOpen] = useState(false);
+
+  const openRemoveAllItemsModal = () => {
+    setIsRemoveAllItemsModalOpen(true);
+  };
+
+  const closeRemoveAllItemsModal = () => {
+    setIsRemoveAllItemsModalOpen(false);
+  };
+
   // 카드결제 눌렀을 때 로딩인디케이터 띄우는 함수
   // 현금결제 눌렀을 때 현금결제 모달 띄우는 함수
-}
 
+  const reducedItems = cartItems.reduce((acc: CartItem[], cartItem: CartItem) => {
+    const sameItem = acc.find((item) => item.id === cartItem.id);
+
+    if (sameItem) {
+      sameItem.count += cartItem.count;
+    } else {
+      acc.push({ ...cartItem });
+    }
+
+    return acc;
+  }, []);
+
+  const openPaymentSelectionModal = () => {
+    setIsPaymentModalOpen(true);
+  };
+
+  const closePaymentSelectionModal = () => {
+    setIsPaymentModalOpen(false);
+  };
+
+  const selectCardPayment = () => {};
+
+  const selectCashPayment = () => {};
+
+  return (
+    <section className={styles.Cart}>
+      <div className={styles.ItemSection}>
+        <ul className={styles.ItemContainer}>
+          {reducedItems.map((item) => (
+            <li className={styles.Item} key={item.id}>
+              <CartItem {...item} removeItem={removeItem} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.ButtonSection}>
+        <div className={styles.Timer}>8초 남음</div>
+        <button className={styles.CancelAllButton} onClick={openRemoveAllItemsModal}>
+          전체 취소
+        </button>
+        <button className={styles.PaymentButton}>결제하기</button>
+      </div>
+      {isRemoveAllItemsModalOpen && (
+        <RemoveAllItemsConfirmationModal closeModal={closeRemoveAllItemsModal} removeAllItems={removeAllItems} />
+      )}
+      {isPaymentModalOpen && (
+        <PaymentSelectionModal
+          closeModal={closePaymentSelectionModal}
+          selectCardPayment={selectCardPayment}
+          selectCashPayment={selectCashPayment}
+        />
+      )}
+    </section>
+  );
+}
 interface CartItemProps {
   id: number;
   name: string;
+  price: number;
   imageSrc: string;
   count: number;
-  price: number;
   removeItem: (id: number) => void;
 }
 
 function CartItem({ id, name, imageSrc, count, price, removeItem }: CartItemProps) {
-  // return (
-  //   <div>
-  //     <img src="" alt="" />
-  //     <div>{item.name}</div>
-  //     <div>{item.count}</div>
-  //   </div>
-  // );
+  return (
+    <div className={styles.ItemContent}>
+      <img className={styles.ItemImage} src={imageSrc} alt={name} />
+      <div>{name}</div>
+      <div>{price}</div>
+      <div className={styles.ItemCount}>{count}</div>
+      <button className={styles.RemoveButton} onClick={() => removeItem(id)}>X</button>
+    </div>
+  );
+}
+
+interface RemoveAllItemsConfirmationModalProps {
+  closeModal: () => void;
+  removeAllItems: () => void;
+}
+
+function RemoveAllItemsConfirmationModal({ closeModal, removeAllItems }: RemoveAllItemsConfirmationModalProps) {
+  const handleConfirmButtonClick = () => {
+    removeAllItems();
+    closeModal();
+  };
+
+  return (
+    <Modal closeModal={closeModal}>
+      <>
+        <div className={styles.ModalContent}>장바구니에 담긴 상품 모두 삭제하시겠습니까?</div>
+        <div className={styles.ButtonContainer}>
+          <button className={styles.ConfirmButton} onClick={handleConfirmButtonClick}>
+            예
+          </button>
+          <button className={styles.CancelButton} onClick={closeModal}>
+            아니오
+          </button>
+        </div>
+      </>
+    </Modal>
+  );
 }
