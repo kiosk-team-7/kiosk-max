a32e2b19511000001fd0a2e519e08b5844f33c2d
Jello <60080167+hjsong333@users.noreply.github.com>
2023-06-28 01:32:32 +0900

    현금 결제 스토리 구현 (#57)
    * feat #52: 금액 투입 기능 및 현금 결제 버튼 활성화
    
    - 금액 투입할 때마다 투입 금액과 주문 금액 비교
    - 투입 금액이 주문금액 이상이면 현금 결제 버튼 활성화
    
    * feat #52: 현금 결제 버튼 클릭 시 서버로 결제 요청을 보내는 기능 구현
    
    * feat #52: 현금 결제 취소 기능 구현
    
    - 현금 결제 모달의 `결제 취소` 버튼을 누르면 결제가 취소되고, 결제 모달을 닫는다.
    - 다시 현금 결제 모달을 열면 투입 금액은 초기화된 상태다.

--- fe/src/components/Payment.tsx
+++ fe/src/components/Payment.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Dim from "./Dim";
 import Modal from "./Modal";
 import OptionButton from "./OptionButton";
@@ -47,13 +47,33 @@ export function PaymentSpinner({ requestPayment }: PaymentSpinnerProps) {
 interface CashPaymentModalProps {
   totalPrice: number;
   closeModal: () => void;
-  requestPayment: () => void;
+  requestPayment: (inputAmount:number) => void;
 }
 
 export function CashPaymentModal({ totalPrice, closeModal, requestPayment }: CashPaymentModalProps) {
   const [inputAmount, setInputAmount] = useState(0);
+  const [isPaymentButtonActive, setIsPaymentButtonActive] = useState(false);
+
+  useEffect(() => {
+    if (inputAmount >= totalPrice) {
+      setIsPaymentButtonActive(true);
+    } else {
+      setIsPaymentButtonActive(false);
+    }
+  }, [inputAmount]);
 
   const inputOptions = [100, 500, 1000, 5000, 10000, 50000];
+  const cancelButtonClassName = `${styles.ConfirmButton} ${styles.CashPaymentCancelButton}`;
+  const confirmButtonClassName = `${styles.ConfirmButton} ${styles.CashPaymentConfirmButton}`;
+
+  const increaseInputAmount = (amount: number) => {
+    setInputAmount((i) => i + amount);
+  };
+
+  const handleConfirmButtonClick = () => {
+    setIsPaymentButtonActive(false);
+    requestPayment(inputAmount);
+  };
 
   return (
     <Modal>
@@ -61,7 +81,7 @@ export function CashPaymentModal({ totalPrice, closeModal, requestPayment }: Cas
         <div className={styles.InputOptionContainer}>
           {inputOptions.map((option) => (
             <div key={option} className={styles.InputOption}>
-              <OptionButton type={"CashInput"} text={option + "원"} onClick={() => {}} />
+              <OptionButton type={"CashInput"} text={option + "원"} onClick={() => increaseInputAmount(option)} />
             </div>
           ))}
         </div>
@@ -74,8 +94,10 @@ export function CashPaymentModal({ totalPrice, closeModal, requestPayment }: Cas
           </div>
         </div>
         <div className={styles.ConfirmButtonContainer}>
-          <button className={`${styles.ConfirmButton} ${styles.CashPaymentCancelButton}`}>결제 취소</button>
-          <button className={`${styles.ConfirmButton} ${styles.CashPaymentConfirmButton}`}>현금 결제하기</button>
+          <button className={cancelButtonClassName} onClick={closeModal}>결제 취소</button>
+          <button className={confirmButtonClassName} onClick={handleConfirmButtonClick} disabled={!isPaymentButtonActive}>
+            현금 결제하기
+          </button>
         </div>
       </>
     </Modal>
