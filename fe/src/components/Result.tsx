b272d7c33e388088a1ef76735a01d80e9d91496f
jsh3418 <albert5428@gmail.com>
2023-06-28 22:54:50 +0900

    feat #55: 결제 실패 화면 구현

--- fe/src/components/Result.tsx
+++ fe/src/components/Result.tsx
@@ -8,26 +8,8 @@ interface ResultPageProps {
 }
 
 export default function Result({ changePage, response }: ResultPageProps) {
-  const isSuccess = response?.status === 200;
-
-  return (
-    <>
-      {isSuccess ? (
-        <Receipt changePage={changePage} response={response.body as ReceiptResponseBody} />
-      ) : (
-        <ErrorPage changePage={changePage} />
-      )}
-    </>
-  );
-}
-
-interface ReceiptProps {
-  changePage: (path: Path) => void;
-  response: ReceiptResponseBody;
-}
-
-function Receipt({ changePage, response }: ReceiptProps) {
   const [count, setCount] = useState(10);
+  const isSuccess = response?.status === 200;
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -46,46 +28,57 @@ function Receipt({ changePage, response }: ReceiptProps) {
   }, [count]);
 
   return (
-    <div className={styles.Display}>
-      <main className={styles.ReceiptContainer}>
-        <h2 className={styles.ReceiptHeader}>
-          주문번호 <span className={styles.OrderNumber}>{String(response.orderNumber).padStart(2, "0")}</span>
-        </h2>
-        <ul className={styles.OrderMenus}>
-          {response.menus.map((menu) => {
-            return (
-              <li key={menu.name}>
-                {menu.name} {menu.count}
-              </li>
-            );
-          })}
-        </ul>
-        <div className={styles.ReceiptInfo}>
-          <div>
-            결제방식 :{" "}
-            {response.paymentType === PaymentType.CASH
-              ? "현금"
-              : response.paymentType === PaymentType.CARD
-              ? "카드"
-              : ""}
-          </div>
-          <div>투입금액 : {response.inputAmount}원</div>
-          <div>총 결제금액 : {response.totalPrice}원</div>
-          <div>잔돈 : {response.remain}원</div>
+    <>
+      <div className={styles.Display}>
+        <main className={styles.Container}>
+          {isSuccess ? (
+            <Receipt response={response.body as ReceiptResponseBody} />
+          ) : (
+            <ErrorPage response={response?.body as ErrorResponseBody} />
+          )}
+        </main>
+        <div className={styles.ReceiptWarningTextContainer}>
+          <div className={styles.ReceiptWarningText}>{"(주의 : 이 화면은 10초 뒤에 자동으로 사라집니다)"}</div>
+          <div>{count}초</div>
         </div>
-      </main>
-      <div className={styles.ReceiptWarningTextContainer}>
-        <div className={styles.ReceiptWarningText}>{"(주의 : 이 화면은 10초 뒤에 자동으로 사라집니다)"}</div>
-        <div>{count}초</div>
       </div>
-    </div>
+    </>
   );
 }
 
-interface ErrorPageProps {
-  changePage: (path: Path) => void;
+function Receipt({ response }: { response: ReceiptResponseBody }) {
+  return (
+    <>
+      <h2 className={styles.Header}>
+        주문번호 <span className={styles.OrderNumber}>{String(response.orderNumber).padStart(2, "0")}</span>
+      </h2>
+      <ul className={styles.OrderMenus}>
+        {response.menus.map((menu) => {
+          return (
+            <li key={menu.name}>
+              {menu.name} {menu.count}
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles.ReceiptInfo}>
+        <div>
+          결제방식 :{" "}
+          {response.paymentType === PaymentType.CASH ? "현금" : response.paymentType === PaymentType.CARD ? "카드" : ""}
+        </div>
+        <div>투입금액 : {response.inputAmount}원</div>
+        <div>총 결제금액 : {response.totalPrice}원</div>
+        <div>잔돈 : {response.remain}원</div>
+      </div>
+    </>
+  );
 }
 
-function ErrorPage(props: ErrorPageProps) {
-  return <></>;
+function ErrorPage({ response }: { response: ErrorResponseBody }) {
+  return (
+    <>
+      <h2 className={styles.Header}>결제 실패</h2>
+      <div>{`사유: ${response.message}`}</div>
+    </>
+  );
 }
