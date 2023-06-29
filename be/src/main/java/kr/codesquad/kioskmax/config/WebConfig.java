c261d02e016a87b3d92d26621348dfbad6fcf357
road-jin <rjswmtk@gmail.com>
2023-06-29 23:37:24 +0900

    fix #74: cors 에러로 인하여 http method 설정 추가

--- be/src/main/java/kr/codesquad/kioskmax/config/WebConfig.java
+++ be/src/main/java/kr/codesquad/kioskmax/config/WebConfig.java
@@ -1,6 +1,7 @@
 package kr.codesquad.kioskmax.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost");
+            .allowedOrigins("http://localhost")
+            .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(),
+                HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name());
     }
 }
